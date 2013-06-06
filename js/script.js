30554fb4e842bff1194f668f1bf84b4a28872850
Andrea Lai <andreatlai@gmail.com>
2013-06-06 21:55:14 +1200

    resolving merge issues

--- js/script.js
+++ js/script.js
@@ -6,24 +6,13 @@
 $(function () {
 
 // 1. Load data
-        
-<<<<<<< HEAD
 	url = "https://spreadsheets.google.com/feeds/list/0AiE7hdFAKPqldE1yOUwzUFh0VkNOVVZ1TWhseGRkdkE/od6/public/values?alt=json-in-script&callback=?"
 	rows = []; // all the deaths logged
-	data_array = [[Date.UTC(2013, 3, 23),1],[Date.UTC(2013, 3, 23),3],[Date.UTC(2013, 3, 24),1],[Date.UTC(2013, 3, 25),5]];
-
-	$.getJSON(url,
-		function(data) {
-		// parse JSON and push data into the list of Spelunky deaths
-=======
-		url = "https://spreadsheets.google.com/feeds/list/0AiE7hdFAKPqldE1yOUwzUFh0VkNOVVZ1TWhseGRkdkE/od6/public/values?alt=json-in-script&callback=?"
-		death_list = []; // all the deaths logged
-		
+	data_array = [[Date.UTC(2013, 3, 23),1],[Date.UTC(2013, 3, 23),3],[Date.UTC(2013, 3, 24),1],[Date.UTC(2013, 3, 25),5]];		
 	
 		$.getJSON(url,
 		function(data) {
 			// parse JSON and push data into the list of Spelunky deaths
->>>>>>> 2080c4a7ae68704a02f087340a59e49c0c7d80af
 			for (i=0;i<data.feed.entry.length;i++) {
 				var entry = data.feed.entry[i];
 				var xlrow = {
