e540e4aab9610aca4175f73a66320c00c0cf76f5
Andrea Lai <andreatlai@gmail.com>
2013-06-08 11:55:52 +1200

    Array parsed, sent to highcharts. Very close
    Don't get why I can plot the string (e.g. [1369440000000, 5.95]), but
    what comes out of the array (test[200]).

--- js/script.js
+++ js/script.js
@@ -8,8 +8,9 @@ $(function () {
 // 1. Load data
 	url = "https://spreadsheets.google.com/feeds/list/0AiE7hdFAKPqldE1yOUwzUFh0VkNOVVZ1TWhseGRkdkE/od6/public/values?alt=json-in-script&callback=?"
 	rows = []; // all the deaths logged
-	data_array = [[Date.UTC(2013, 3, 23),1],[Date.UTC(2013, 3, 23),3],[Date.UTC(2013, 3, 24),1],[Date.UTC(2013, 3, 25),5]];		
-	
+	data_array = [[Date.UTC(2013, 3, 23),1],[Date.UTC(2013, 3, 23),3],[Date.UTC(2013, 3, 24),1],[Date.UTC(2013, 3, 25),5]];	
+	test = [];
+		
 		$.getJSON(url,
 		function(data) {
 			// parse JSON and push data into the list of Spelunky deaths
@@ -22,12 +23,27 @@ $(function () {
 				death: entry["gsx$death"].$t
 				};
 			rows.push(xlrow);
+			
+			}
+			
+			//create data series for highchartsy
+			for (i=0;i<rows.length;i++){
+				//date as Year/Month/Day
+				parts = rows[i].date.match(/(\d+)/g);
+				dateUTC = Date.UTC(parts[2], parts[0]-1, parts[1]);
+				minutes = parseFloat(rows[i].time);
+				test.push([dateUTC, minutes]);
 			}
 		});
 
+		//test.push(rows[0].time);
+		
 //2. Parse spreadsheet rows to format for charting: [Date.UTC(Year, Month-1, Day), time]
 // for i in rows, format, data_array.push(formatted)
 
+		//parse date string
+		
+		
 		//data_array.push([3,4]);
 
 //3. Initiate chart		
@@ -67,7 +83,8 @@ $(function () {
             
             series: [{
                 //data: [[Date.UTC(2013, 3, 23),1],[Date.UTC(2013, 3, 24),1],[Date.UTC(2013, 3, 25),5]]
-				data: data_array
+				data: [data_array[0], data_array[1], data_array[2], [Date.UTC(2013,3,6),5.1], [1365206400000, 5.2], test[200]]
+				// test[200] = [1369440000000, 5.95], which plots fine. I don't get it!
 			}]
         });
     });
