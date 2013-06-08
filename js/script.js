33bf76b89e9e3f46dc525f44dee3ee391b4bd6ec
Nikki Lee <nikki@nicoleblee.com>
2013-06-07 22:03:27 -0700

    cleaning up formatting

--- js/script.js
+++ js/script.js
@@ -6,47 +6,49 @@
 $(function () {
 
 // 1. Load data
-	url = "https://spreadsheets.google.com/feeds/list/0AiE7hdFAKPqldE1yOUwzUFh0VkNOVVZ1TWhseGRkdkE/od6/public/values?alt=json-in-script&callback=?"
-	rows = []; // all the deaths logged
-	data_array = [[Date.UTC(2013, 3, 23),1],[Date.UTC(2013, 3, 23),3],[Date.UTC(2013, 3, 24),1],[Date.UTC(2013, 3, 25),5]];	
-	test = [];
+	var url = "https://spreadsheets.google.com/feeds/list/0AiE7hdFAKPqldE1yOUwzUFh0VkNOVVZ1TWhseGRkdkE/od6/public/values?alt=json-in-script&callback=?";
+	var rows = []; // all the deaths logged
+	var data_array = [[Date.UTC(2013, 3, 23),1],[Date.UTC(2013, 3, 23),3],[Date.UTC(2013, 3, 24),1],[Date.UTC(2013, 3, 25),5]];	
+	var test = [];
 		
-		$.getJSON(url,
-		function(data) {
-			// parse JSON and push data into the list of Spelunky deaths
-			for (i=0;i<data.feed.entry.length;i++) {
-				var entry = data.feed.entry[i];
-				var xlrow = {
+	$.getJSON(url,
+	function(data) {
+		// parse JSON and push data into the list of Spelunky deaths
+		for (i=0;i<data.feed.entry.length;i++) {
+			var entry = data.feed.entry[i];
+			var xlrow = {
 				date: entry["gsx$date"].$t,
 				time: entry["gsx$minutes"].$t,
 				level: entry["gsx$level"].$t,
 				death: entry["gsx$death"].$t
 				};
-			rows.push(xlrow);
-			
-			}
-			
-			//create data series for highchartsy
-			for (i=0;i<rows.length;i++){
-				//date as Year/Month/Day
-				parts = rows[i].date.match(/(\d+)/g);
-				dateUTC = Date.UTC(parts[2], parts[0]-1, parts[1]);
-				minutes = parseFloat(rows[i].time);
-				test.push([dateUTC, minutes]);
-			}
-		});
+
+		rows.push(xlrow);
+		}
+		
+		//create data series for highchartsy
+		for (var i=0;i<rows.length;i++){
+			//date as Year/Month/Day
+			parts = rows[i].date.match(/(\d+)/g);
+			dateUTC = Date.UTC(parts[2], parts[0]-1, parts[1]);
+			minutes = parseFloat(rows[i].time);
+			test.push([dateUTC, minutes]);
+		}
+
+		// TODO: add the plotting function here
+	});
 
 		//test.push(rows[0].time);
 		
-//2. Parse spreadsheet rows to format for charting: [Date.UTC(Year, Month-1, Day), time]
-// for i in rows, format, data_array.push(formatted)
+		//2. Parse spreadsheet rows to format for charting: [Date.UTC(Year, Month-1, Day), time]
+		// for i in rows, format, data_array.push(formatted)
 
 		//parse date string
 		
 		
 		//data_array.push([3,4]);
 
-//3. Initiate chart		
+	//3. Initiate chart		
 	$('#container').highcharts({
 				chart: {
                 type: 'scatter'
